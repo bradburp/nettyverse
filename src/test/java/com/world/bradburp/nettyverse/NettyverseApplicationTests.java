a5af46e1ef9192e537155a9e29f41740a065257a
bradburp <paul.bradbury@live.co.uk>
2023-11-13 18:18:37 +0000

    Initial commit

---
+++ src/test/java/com/world/bradburp/nettyverse/NettyverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.bradburp.nettyverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
